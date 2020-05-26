cf133c2f7fa48f7a32b258aec7d5ce3a344aa380
fearless-command-2358 <iuriseara@gmail.com>
2020-05-26 20:29:54 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -5,4 +5,4 @@ def meal_choice(veg1, veg2, protein = 'tofu')
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-return meal_choice("broccoli", "macoroni", "roast beef")
+meal_choice("broccoli", "macoroni", "roast beef")
